7e2b54836f64fb39b495ab658b9d9409d129e999
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2022-12-12 19:20:03 +0530

    Delete class36-StringStrip-CipherSchools.py

--- class36-StringStrip-CipherSchools.py
+++
@@ -1,7 +0,0 @@
-name = "             Harshit            "
-dots = "..................."
-
-print(name + dots)
-print(name.lstrip() + dots)#removes left spaces
-print(name.rstrip() + dots)#removes right spaces
-print(name.strip() + dots)
